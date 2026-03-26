d73b53fb7619adb1cddce57e9bb847e04de32839
wuwenjincat <tomatocat1130@qq.com>
2026-03-26 23:33:26 +0800

    2026.3.26 提交代码

---
+++ leetcode-hot-100/src/main/java/problem207/Solution.java
@@ -0,0 +1,6 @@
+package problem207;
+
+//dfs深度优先搜索
+public class Solution {
+
+}
---
+++ leetcode-hot-100/src/main/java/problem70/Solution.java
@@ -0,0 +1,18 @@
+package problem70;
+
+public class Solution {
+    public int climbStairs(int n){
+        //只有一阶，只有一种走法
+       if(n==1) return 1;
+       // dp[i]表示爬到第i阶的方法数
+       int[] dp=new int[n+1];
+
+       dp[0]=1;//0阶，1种
+       dp[1]=1;//1阶，1种
+
+       for(int i=2;i<=n;i++){
+           dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
+    }
+}
